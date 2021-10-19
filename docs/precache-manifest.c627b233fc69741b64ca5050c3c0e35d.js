4628e8a565f5464a0d565e28152906fb5af25bbd
Graham White <gwhite@uk.ibm.com>
2021-10-19 21:48:21 +0100

    Move to new VueJS 3 based site

---
+++ docs/precache-manifest.c627b233fc69741b64ca5050c3c0e35d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf2ee5ee96d797abe76",
+    "url": "/css/about.07a44037.css"
+  },
+  {
+    "revision": "b839d5319b7b74f88340",
+    "url": "/css/app.9438a0ab.css"
+  },
+  {
+    "revision": "dee3444b2ae81270ff6cdd1e68b414e8",
+    "url": "/img/green.dee3444b.jpg"
+  },
+  {
+    "revision": "df3f2c653e5fa3ee1f58033a3e400ca7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cf2ee5ee96d797abe76",
+    "url": "/js/about.ea50f372.js"
+  },
+  {
+    "revision": "b839d5319b7b74f88340",
+    "url": "/js/app.70c56ddc.js"
+  },
+  {
+    "revision": "c5593aeba359380a68ea",
+    "url": "/js/chunk-vendors.966de607.js"
+  },
+  {
+    "revision": "2d2eda57064053dfe72bd6b8afde6204",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
